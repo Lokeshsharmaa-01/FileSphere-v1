9b483a2353c8f3cccd592fd6a53e6ddcaedaf0c2
Lokesh <iamlokeshsharmaa@gmail.com>
2026-01-23 10:40:50 +0530

    preview functionality added

--- app.js
+++ app.js
@@ -3,27 +3,17 @@ import { open, readdir } from "fs/promises";
 import { readFile } from "node:fs/promises";
 
 const server = http.createServer(async (req, res) => {
-  const files = await readdir("./storage");
-  let dynamicHTML = "";
-  const html = await readFile("./index.html", "utf-8");
-  files.forEach((item) => {
-    dynamicHTML += `<a href=${item}>${item}</a><br>`;
-  });
+  if(req.url === '/favicon.ico') return res.end('no favicon')
+  console.log(req.url);
+  
   if (req.url === "/") {
-    res.end(html.replace("${dynamicHTML}", dynamicHTML));
+    serveDir(req,res);
   } else {
     try {
       const fileHandle = await open(`./storage${decodeURIComponent(req.url)}`);
       const stats = await fileHandle.stat();
       if (stats.isDirectory()) {
-        const files = await readdir(`./storage${req.url}`);
-        let dynamicHTML = "";
-          const html = await readFile("./index.html", "utf-8");
-
-        files.forEach((item) => {
-          dynamicHTML += `<a href=${req.url}/${item}>${item}</a><br>`;
-        });
-        res.end(html.replace('${dynamicHTML}',dynamicHTML))
+        serveDir(req,res);
       } else {
         const readStream = fileHandle.createReadStream();
         readStream.pipe(res);
@@ -34,6 +24,18 @@ const server = http.createServer(async (req, res) => {
     }
   }
 });
+
+async function serveDir(req,res){
+  const files = await readdir(`./storage${req.url}`);
+  let dynamicHTML = "";
+  const html = await readFile("./index.html", "utf-8");
+  files.forEach((item) => {
+    dynamicHTML += `${item}<a href="${req.url === '/' ? "": req.url}/${item}?action=open">Open</a> ${item}<a href="${req.url === '/' ? "": req.url}/${item}?action=download">Download</a><br>`;
+
+  })
+  res.end(html.replace('${dynamicHTML}',dynamicHTML))
+
+}
 
 server.listen(4000, "0.0.0.0", () => {
   console.log("server is running on port 4000");
