a7145ce11145cee74c259569003a678b9a6d373a
Lokesh <iamlokeshsharmaa@gmail.com>
2026-01-28 11:11:32 +0530

    download and preview functionality added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import http from "node:http";
 import { open, readdir } from "fs/promises";
 import { readFile } from "node:fs/promises";
+import mime from 'mime-types'
 
 const server = http.createServer(async (req, res) => {
   if(req.url === '/favicon.ico') return res.end('no favicon')
@@ -10,12 +11,24 @@ const server = http.createServer(async (req, res) => {
     serveDir(req,res);
   } else {
     try {
-      const fileHandle = await open(`./storage${decodeURIComponent(req.url)}`);
+        const [url,queryString] = req.url.split('?')
+      const queryParams = {}
+      queryString.split('&').forEach((pair) =>{
+        const [key,value] = pair.split('=')
+        queryParams[key] = value;
+      })
+      
+      const fileHandle = await open(`./storage${decodeURIComponent(url)}`);
       const stats = await fileHandle.stat();
       if (stats.isDirectory()) {
         serveDir(req,res);
       } else {
         const readStream = fileHandle.createReadStream();
+        res.setHeader('Content-Type',mime.contentType(url.slice(1)))
+        res.setHeader('Content-Length',stats.size)
+        if(queryParams.action === 'download'){
+          res.setHeader('Content-Disposition',`attachment;filename = "${url.slice(1)}"`)
+      }
         readStream.pipe(res);
       }
     } catch (err) {
@@ -26,11 +39,12 @@ const server = http.createServer(async (req, res) => {
 });
 
 async function serveDir(req,res){
+  const [url,queryString] = req.url.split('?')
   const files = await readdir(`./storage${req.url}`);
   let dynamicHTML = "";
   const html = await readFile("./index.html", "utf-8");
   files.forEach((item) => {
-    dynamicHTML += `${item}<a href="${req.url === '/' ? "": req.url}/${item}?action=open">Open</a> ${item}<a href="${req.url === '/' ? "": req.url}/${item}?action=download">Download</a><br>`;
+    dynamicHTML += `${item}<a href="${req.url === '/' ? "": req.url}/${item}?action=open">Open</a><a href="${req.url === '/' ? "": req.url}/${item}?action=download">Download</a><br>`;
 
   })
   res.end(html.replace('${dynamicHTML}',dynamicHTML))
