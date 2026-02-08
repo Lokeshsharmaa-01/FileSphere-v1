05f9ceab2fa6a734e59237f279faddaf74642e4f
Lokesh <iamlokesh2024gmail.com>
2026-02-08 22:08:54 +0530

    update code

--- client/filesphere/src/App.jsx
+++ client/filesphere/src/App.jsx
@@ -1,32 +1,48 @@
-import { useState } from 'react'
-import './App.css'
-import { useEffect } from 'react'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
- 
-  const [diritems,setDiritems] = useState([])
-  async function getdiritems() {
-    const response = await fetch('http://192.168.0.198:4000/')
+  const [directoryItems, setDirectoryItems] = useState([]);
+  const [progress, setProgress] = useState(0);
+
+  async function getDirectoryItems() {
+    const response = await fetch("http://192.168.0.198/");
+  
     const data = await response.json();
-    console.log(data);
-    
-    setDiritems(data);
-    
+    setDirectoryItems(data);
   }
-  useEffect(()=>{
-    getdiritems()
-  },[])
+  useEffect(() => {
+    getDirectoryItems();
+  }, []);
+
+  async function handleChange(e) {
+    const file = e.target.files[0];
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://192.168.0.198/", true);
+    xhr.setRequestHeader("filename", file.name);
+    xhr.addEventListener("load", () => {
+      console.log(xhr.response);
+    });
+    xhr.upload.addEventListener("progress", (e) => {
+      const totalProgress = (e.loaded / e.total) * 100;
+      setProgress(totalProgress.toFixed(2));
+    });
+    xhr.send(file);
+  }
   return (
     <>
-    <h1> My Files</h1>
-      {
-        diritems.map((item)=>(
-         
-          <div>{item} <a href={`http:/192.168.0.198:4000/${item}?action=open`}>Open</a> <a href={`http:/192.168.0.198:4000/${item}?action=download`}>Download</a> <br/></div>
-        ))
-      }
+      <h1>My Files</h1>
+      <input type="file" onChange={handleChange} />
+      <p>Progress: {progress}%</p>
+      {directoryItems.map((item, i) => (
+        <div key={i}>
+          {item} <a href={`http://192.168.0.198/${item}?action=open`}>Open</a>{" "}
+          <a href={`http://192.168.0.198/${item}?action=download`}>Download</a>
+          <br />
+        </div>
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- server/app.js
+++ server/app.js
@@ -1,61 +1,70 @@
-import http from "node:http";
-import { open, readdir } from "fs/promises";
-import { readFile } from "node:fs/promises";
-import mime from 'mime-types'
-import { json } from "node:stream/consumers";
+import { createWriteStream } from "fs";
+import { open, readdir, readFile } from "fs/promises";
+import http from "http";
+import mime from "mime-types";
 
 const server = http.createServer(async (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin',"*")
-  if(req.url === '/favicon.ico') return res.end('no favicon')
-  console.log(req.url);
-  
-  if (req.url === "/") {
-    serveDir(req,res);
-  } else {
-    try {
-        const [url,queryString] = req.url.split('?')
-      const queryParams = {}
-      queryString?.split('&').forEach((pair) =>{
-        const [key,value] = pair.split('=')
-        queryParams[key] = value;
-      })
-      
-      const fileHandle = await open(`./storage${decodeURIComponent(url)}`);
-      const stats = await fileHandle.stat();
-      if (stats.isDirectory()) {
-        serveDir(req,res);
-      } else {
-        const readStream = fileHandle.createReadStream();
-        res.setHeader('Content-Type',mime.contentType(url.slice(1)))
-        res.setHeader('Content-Length',stats.size)
-        if(queryParams.action === 'download'){
-          res.setHeader('Content-Disposition',`attachment;filename = "${url.slice(1)}"`)
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  console.log(req.method);
+  if (req.method === "GET") {
+    if (req.url === "/favicon.ico") return res.end("No favicon.");
+    if (req.url === "/") {
+      serveDirectory(req, res);
+    } else {
+      try {
+        const [url, queryString] = req.url.split("?");
+        const queryParam = {};
+        queryString?.split("&").forEach((pair) => {
+          const [key, value] = pair.split("=");
+          queryParam[key] = value;
+        });
+        console.log(queryParam);
+
+        const fileHandle = await open(`./storage${decodeURIComponent(url)}`);
+        const stats = await fileHandle.stat();
+        if (stats.isDirectory()) {
+          serveDirectory(req, res);
+        } else {
+          const readStream = fileHandle.createReadStream();
+          res.setHeader("Content-Type", mime.contentType(url.slice(1)));
+          res.setHeader("Content-Length", stats.size);
+          if (queryParam.action === "download") {
+            res.setHeader(
+              "Content-Disposition",
+              `attachment; filename="${url.slice(1)}"`
+            );
+          }
+          readStream.pipe(res);
+        }
+      } catch (err) {
+        console.log(err.message);
+        res.end("Not Found!");
       }
-        readStream.pipe(res);
-      }
-    } catch (err) {
-      console.log(err.message);
-      res.end("not found");
     }
+  } else if (req.method === "OPTIONS") {
+    res.end("OK");
+  } else if (req.method === "POST") {
+    const writeStream = createWriteStream(`./storage/${req.headers.filename}`);
+    let count = 0;
+    req.on("data", (chunk) => {
+      count++;
+      writeStream.write(chunk);
+    });
+    req.on("end", () => {
+      console.log(count);
+      res.end("File uploaded on the server");
+    });
   }
 });
 
-async function serveDir(req,res){
-  res.setHeader('Access-Control-Allow-Origin',"*")
-  const [url,queryString] = req.url.split('?')
-  const files = await readdir(`./storage${url}`);
-  res.setHeader('Content-Type',"application/json")
-  // let dynamicHTML = "";
-  // const html = await readFile("./index.html", "utf-8");
-  // files.forEach((item) => {
-  //   dynamicHTML += `${item}<a href="${req.url === '/' ? "": req.url}/${item}?action=open">Open</a><a href="${req.url === '/' ? "": req.url}/${item}?action=download">Download</a><br>`;
-
-  // })
-  // res.end(html.replace('${dynamicHTML}',dynamicHTML))
-  res.end(JSON.stringify(files))
-
+async function serveDirectory(req, res) {
+  const [url] = req.url.split("?");
+  const itemsList = await readdir(`./storage${url}`);
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify(itemsList));
 }
 
-server.listen(4000, "0.0.0.0", () => {
-  console.log("server is running on port 4000");
+server.listen(80, "0.0.0.0", () => {
+  console.log("Server started");
 });
